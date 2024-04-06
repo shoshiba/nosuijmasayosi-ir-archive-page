fea9e0b587c7dae960f00c9b1208f5d13472dd49
sig49san <sibahara3@gmail.com>
2024-04-06 18:13:52 +0900

    csvからテーブルを表示できるようにした

---
+++ app/Component/util/parseCsv.ts
@@ -0,0 +1,19 @@
+// utils/parseCsv.js
+import Papa from 'papaparse';
+
+export const parseCsv = async (path) => {
+  const response = await fetch(path);
+  const reader = response.body.getReader();
+  const result = await reader.read(); // raw array
+  const decoder = new TextDecoder('utf-8');
+  const csv = decoder.decode(result.value); // the csv text
+  return new Promise((resolve, reject) => {
+    Papa.parse(csv, {
+      header: true,
+      complete: (results) => {
+        resolve(results.data);
+      },
+      error: (error) => reject(error.message),
+    });
+  });
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
-import { BasicTable } from "./Component/BasicTable"
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import { BasicTable } from "./component/BasicTable"
+import { parseCsv} from './component/util/parseCsv'
+import  DataTable  from './component/DataTable'
+
+const columns = [
+  { Header: 'JYUNI', accessor: 'JYUNI' },
+  { Header: 'PLAYER NAME', accessor: 'PLAYER NAME' },
+  { Header: '☆9↓', accessor: '☆9↓' },
+  { Header: '☆10', accessor: '☆10' },
+  { Header: '☆11', accessor: '☆11' },
+  { Header: '☆12', accessor: '☆12' },
+  { Header: 'TOTAL', accessor: 'TOTAL' },
+  { Header: 'RATE', accessor: 'RATE' },
+  { Header: 'RANK', accessor: 'RANK' },
+  { Header: 'RANK+', accessor: 'RANK+' },
+  { Header: 'NEXT', accessor: 'NEXT' },
+];
+
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await parseCsv('/csv/gm_result.csv');
+      console.log(result)
+      setData(result);
+    }
+    fetchData();
+  })
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <BasicTable />
+      <DataTable columns = {columns} data= {data} />
     </main>
 
   );
